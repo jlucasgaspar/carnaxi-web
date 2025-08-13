1934913e24b5cd0b481a247a542431a4bb0ac84f
Jose Lucas <jose.gaspar@bizrate.com>
2025-08-13 18:32:50 -0300

    refactor RankForm

---
+++ src/components/RankForm.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { FormEvent, useState } from "react";
+import { toast } from "sonner";
+import { LoaderCircle } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRankedAssociations } from "@/contexts/rankedAssociations";
+
+export function RankForm() {
+  const [name, setName] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  const { rankedAssociations } = useRankedAssociations();
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!name) {
+      toast.error("Nome é obrigatório");
+      return;
+    }
+
+    if (rankedAssociations.length !== 12) {
+      toast.error("Você deve classificar 12 escolas antes de enviar.");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const res = await fetch('/api/jackpot', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          rankedAssociations,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data?.errorMessage || "Algo deu errado na requisição");
+        return;
+      }
+
+      console.log(data);
+      toast.success("Bolão enviado!");
+    } catch (error) {
+      console.log(error);
+      toast.error("Algo deu errado ao enviar o bolão");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input
+        name="myname"
+        placeholder="Seu nome"
+        className="mt-7 mb-3 h-12"
+        onChange={event => setName(event.currentTarget.value)}
+        value={name}
+      />
+      <Button
+        type="submit"
+        size="lg"
+        className="w-full h-12 text-lg font-semibold"
+        disabled={!name || isLoading}
+      >
+        {isLoading && <LoaderCircle className="animate-spin" />}
+        {isLoading ? 'Carregando...' : 'Salvar'}
+      </Button>
+    </form>
+  )
+}
